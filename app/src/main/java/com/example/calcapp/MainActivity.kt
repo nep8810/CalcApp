dcfaae084f3e8e9a22851ab790b33382bec639b9
nep8810 <>
2020-09-10 20:54:03 +0900

    四則計算アプリ

--- app/src/main/java/com/example/calcapp/MainActivity.kt
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -5,20 +5,39 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.widget.EditText
+import android.widget.TextView
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
     }
 
+    //onClickメソッドでSecondActivityに遷移させる
     override fun onClick(v: View?) {
+        //Intentのインスタンスを生成
+        //コンストラクタの第一引数はcontextなのでActivity自身,第2引数は遷移させたいActivityのクラスを指定
         val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra("VALUE1", 10)
-        intent.putExtra("VALUE2", 20)
+
+        intent.putExtra("VALUE1",  "@+id/editText1")
+        intent.putExtra("VALUE2", "@+id/editText2")
+
+        //生成したIntentを引数にしてstartActivityメソッドを呼び出すことで遷移
         startActivity(intent)
     }
 }
+
--- app/src/main/java/com/example/calcapp/SecondActivity.kt
+++ app/src/main/java/com/example/calcapp/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.example.calcapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,10 +12,18 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val value1 = intent.getIntExtra("VALUE1", 0)
+        val value1 = intent.getIntExtra("VALUE1",  0)
         val value2 = intent.getIntExtra("VALUE2", 0)
 
-        textView.text = "${value1 + value2}"
+
+        when(R.id){
+            R.id.button1 -> textView.text = "${value1 + value2}"
+            R.id.button2 -> textView.text = "${value1 - value2}"
+            R.id.button3 -> textView.text = "${value1 * value2}"
+            R.id.button4-> textView.text = "${value1 / value2}"
+            else -> textView.text ="何か数値を入力してください"
+
+        }
+
     }
 }
-
